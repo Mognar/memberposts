9622d77ff63f2507756be052081efae96ac67b33
Mognar <philipbgorman@gmail.com>
2017-06-22 12:30:07 +0100

    Add files via upload

--- Membersposts.py
+++ Membersposts.py
@@ -1,22 +1,10 @@
 
 # coding: utf-8
 
-# In[1]:
 
-get_ipython().system(u'pip install pyslet')
-
-
-# In[2]:
 
 import time
 
-
-# In[3]:
-
-get_ipython().system(u'pip install tweepy')
-
-
-# In[4]:
 
 import json
 import tweepy
@@ -33,13 +21,13 @@ auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
 
-# In[5]:
+# In[ ]:
 
 from pyslet.odata2.client import Client
 c = Client("http://data.parliament.uk/membersdataplatform/open/OData.svc")
 
 
-# In[6]:
+# In[ ]:
 
 import pyslet.odata2.core as core
 
@@ -56,53 +44,103 @@ class ReplyToTweet(StreamListener):
     def on_data(self, data):
         print data
         tweet = json.loads(data.strip())
-        
+
         retweeted = tweet.get('retweeted')
         from_self = tweet.get('user',{}).get('id_str','') == 854276747096973313
+        tweetText = tweet.get('text')
 
-        if retweeted is not None and not retweeted and not from_self:
-
+        if retweeted is not None and (not retweeted) and ('RT @' not in tweetText) and (not from_self):
+            #tweet.text.encode('utf8')
             tweetId = tweet.get('id_str')
             screenName = tweet.get('user',{}).get('screen_name')
             tweetText = tweet.get('text')
-            
-            y = str(tweetText[17:])
-            print y
+            print tweetText
+
+            if "#" in tweetText:
+                str2="#"
+                y = str(tweetText[10:])
+                print y
+                z = y.rindex(str2)
+                print z
+                tm = str(y[:z])
+                print tm
+                tr = tm.strip(' ')
+            else:
+                tm = str(tweetText[10:])
+                print tm
+                tr = tm.strip(' ')
+
+            words = tr.split()
+            print words
+            tr1 = words[0]
+            tr2 = words[1]
+            print tr1
+            print tr2
+
             m=c.feeds['Members'].OpenCollection()
             ordering=core.CommonExpression.OrderByFromString("NameDisplayAs Desc")
             m.set_orderby(ordering)
 
-            #Add in a filter "+y+"
-            odfilter = core.CommonExpression.from_str("substringof('"+y+"',NameDisplayAs)")
+            odfilter = core.CommonExpression.from_str("startswith('"+tr1+"',Forename) and startswith('"+tr2+"',Surname)")
             m.set_filter(odfilter)
 
-            #View the response
             for p in m.itervalues():
                 print(p.key(), p['NameDisplayAs'].value)
-                mem = str(p['NameDisplayAs'].value)  
+                mem = str(p['NameDisplayAs'].value)
                 print mem
                 z = int(p.key())
                 print z
 
                 n=c.feeds['Members'].OpenCollection()
-                mcm=n[z]['MemberGovernmentPosts'].OpenCollection()
-                for k in mcm.keys():
-                #Get the Government Post details
-                    with mcm[k]['GovernmentPost'].OpenCollection() as mcmc:
-                        for k2,v2 in mcmc[mcmc.keys()[0]].data_items():
-                            #if k2=='NameDisplayAs' or k2=='HansardName' or k2=='StartDate':
-                            if k2=='HansardName'or k2=='StartDate':
-                                tw = str(v2.value)
-                                print tw
-                            #else: 
-                                #tw = "No posts held"
-                            #print(v2.value)
+                if ("#govposts" in tweetText) and (not "#committees" in tweetText):
+                    mcm=n[z]['MemberGovernmentPosts'].OpenCollection()
+                    for k in mcm.keys():
+                    #Get the Government Post details
+                        with mcm[k]['GovernmentPost'].OpenCollection() as mcmc:
+                            for k2,v2 in mcmc[mcmc.keys()[0]].data_items():
+                                if k2=='Name':
+                                #if k2=='HansardName' :
+                                    tw = str(v2.value)
+                                    print tw
+                                #print(v2.value)
 
-                    #print('------')
-                    #api.update_status(repl + " " + tw + " " + tim)
-                    replyText = str("@"+screenName + " " + mem + " " + tw)
+                                #print('------')
+                                #api.update_status(repl + " " + tw + " " + tim)
+                            replyText = str("@"+screenName + " " + mem + " " + tw)
+                            print replyText
+                            try:
+                                api.update_status(status=replyText, in_reply_to_status_id = tweetId)
+                            except tweepy.TweepError as e:
+                                pass
+                elif ("#committees" in tweetText) and (not "#govposts" in tweetText):
+                    mcm=n[z]['MemberCommittees'].OpenCollection()
+                    for k in mcm.keys():
+                    #Get the Government Post details
+                        with mcm[k]['Committee'].OpenCollection() as mcmc:
+                            for k2,v2 in mcmc[mcmc.keys()[0]].data_items():
+                                if k2=='Name':
+                                #if k2=='HansardName' :
+                                    tw = str(v2.value)
+                                    print tw
+                                #print(v2.value)
+
+                                #print('------')
+                                #api.update_status(repl + " " + tw + " " + tim)
+                            replyText = str("@"+screenName + " " + mem + " " + tw)
+                            print replyText
+                            try:
+                                api.update_status(status=replyText, in_reply_to_status_id = tweetId)
+                            except tweepy.TweepError as e:
+                                pass
+                else:
+                    tv = n[z]['DateOfWrit'].value
+                    tw = str(tv)
+                    replyText = str("@"+screenName + " " + mem + " Date of writ: " + tw)
                     print replyText
-                    api.update_status(status=replyText, in_reply_to_status_id = tweetId)
+                    try:
+                        api.update_status(status=replyText, in_reply_to_status_id = tweetId)
+                    except tweepy.TweepError as e:
+                        pass
                 #api.update_status(repl + " " + tw)
                 #print(repl + " " + tw)
 
@@ -110,86 +148,3 @@ if __name__ == '__main__':
     streamListener = ReplyToTweet()
     twitterStream = Stream(auth, streamListener)
     twitterStream.userstream(_with='user')
-
-
-# 
-#     #for tweet in tweepy.Cursor(api.search,
-#                                #q='@parldivisionbot').items(1):
-#     #for tweet in tweepy.StreamListener():
-#         #x = str(tweet.text)
-#         #repl = str("@"+tweet.user.screen_name)
-#         #tim = str(tweet.id)
-#         #print x
-#             y = str(tweetText[17:])
-#             print y
-#             ordering=core.CommonExpression.OrderByFromString("NameDisplayAs Desc")
-#             m.set_orderby(ordering)
-# 
-#             #Add in a filter "+y+"
-#             odfilter = core.CommonExpression.from_str("substringof('"+y+"',NameDisplayAs)")
-#             m.set_filter(odfilter)
-# 
-#             #View the response
-#             for p in m.itervalues():
-#                 print(p.key(), p['NameDisplayAs'].value)
-#                 z = int(p.key())
-#                 print z
-# 
-#             n=c.feeds['Members'].OpenCollection()
-#             mcm=n[z]['MemberGovernmentPosts'].OpenCollection()
-#             for k in mcm.keys():
-#                 #Get the Committee details
-#                 with mcm[k]['GovernmentPost'].OpenCollection() as mcmc:
-#                     for k2,v2 in mcmc[mcmc.keys()[0]].data_items():
-#                         #if k2=='NameDisplayAs' or k2=='HansardName' or k2=='StartDate':
-#                         if k2=='HansardName':
-#                             #or v2.value is None:
-#                             tw = str(v2.value)
-#                         else: 
-#                             tw = "No posts held"
-#                         print(v2.value)
-#                 #print('------')
-#             try:
-#                 #api.update_status(repl + " " + tw + " " + tim)
-#                 api.update_status(repl + " " + tw, in_reply_to_status_id = tim)
-#             except tweepy.error.TweepError:
-#                 pass
-#             #api.update_status(repl + " " + tw)
-#             #print(repl + " " + tw)
-# 
-# if __name__ == '__main__':
-#     streamListener = ReplyToTweet()
-#     twitterStream = Stream(auth, streamListener)
-#     twitterStream.userstream(_with='user')
-#         
-
-#     y = str(x[17:])
-#     print y
-#     ordering=core.CommonExpression.OrderByFromString("NameDisplayAs Desc")
-#     m.set_orderby(ordering)
-# 
-#     #Add in a filter "+y+"
-#     odfilter = core.CommonExpression.from_str("substringof('"+y+"',NameDisplayAs)")
-#     m.set_filter(odfilter)
-# 
-#     #View the response
-#     for p in m.itervalues():
-#         print(p.key(), p['NameDisplayAs'].value)
-#         z = int(p.key())
-#         print z
-
-#     n=c.feeds['Members'].OpenCollection()
-#     mcm=n[z]['MemberGovernmentPosts'].OpenCollection()
-#     for k in mcm.keys():
-#         #Get the Committee details
-#         with mcm[k]['GovernmentPost'].OpenCollection() as mcmc:
-#             for k2,v2 in mcmc[mcmc.keys()[0]].data_items():
-#                 #if k2=='NameDisplayAs' or k2=='HansardName' or k2=='StartDate':
-#                 if k2=='HansardName':
-#                     #or v2.value is None:
-#                     tw = str(v2.value)
-#                     print(v2.value)
-#         #print('------')
-
-# #api.update_status(repl + " " + tw)
-# print(repl + " " + tw)
