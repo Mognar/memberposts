51a9af44fe181a54340ef18b822a8a9944ed433f
Mognar <philipbgorman@gmail.com>
2018-06-15 11:52:41 +0100

    Update Membersposts.py

--- Membersposts.py
+++ Membersposts.py
@@ -1,5 +1,3 @@
-
-
 # coding: utf-8
 
 # In[10]:
@@ -103,131 +101,46 @@ class ReplyToTweet(StreamListener):
             
             
             
-			stub='https://api.parliament.uk'.strip('/')
-			url1='{}/odata/Person?$filter=endswith(PersonFamilyName,%27{}%27)%20and%20startswith(PersonGivenName,%27{}%27)'.format(stub,tr2,tr1)
-			
-			r=requests.get(url1)
-			r1=r.json()
-			r2=r1['value'][0]['LocalId']
-			if ("#govposts" in tweetText) and (not "#committees" in tweetText):
-				url2='https://api.parliament.uk/odata/Person(%27{}%27)/PersonHasIncumbency?$expand=IncumbencyHasPosition'.format(r2)
-				r3=requests.get(url2)
-				r4=r3.json()
-				for i in r4['value']:
-				 try:
-					d = datetime.strptime(i['IncumbencyStartDate'], "%Y-%m-%dT%H:%M:%SZ")
-					datum = d.strftime("%d/%m/%Y")
-					tw = i['IncumbencyHasPosition']['PositionName'] + ", started:" + datum
-			
-				 except TypeError:
-					pass
+        stub='https://api.parliament.uk'.strip('/')
+        url1='{}/odata/Person?$filter=endswith(PersonFamilyName,%27{}%27)%20and%20startswith(PersonGivenName,%27{}%27)'.format(stub,tr2,tr1)
 
-									#print('------')
-									#api.update_status(repl + " " + tw + " " + tim)
-				replyText = str("@"+screenName + " " + tw)
-				print replyText
-				try:
-					api.update_status(status=replyText, in_reply_to_status_id = tweetId)
-				except tweepy.TweepError as e:
-					pass
-			else:
-				#tv = n[z]['DateOfWrit'].value
-				#tw = str(tv)
-			replyText = str("@"+screenName + " I'll need a hashtag to tell you more about " + mem + " . #committees or #govposts")
-			print replyText
-			try:
-				api.update_status(status=replyText, in_reply_to_status_id = tweetId)
-			except tweepy.TweepError as e:
-				pass
-			#api.update_status(repl + " " + tw)
-			#print(repl + " " + tw)
+        r=requests.get(url1)
+        r1=r.json()
+        r2=r1['value'][0]['LocalId']
+        if ("#govposts" in tweetText) and (not "#committees" in tweetText):
+            url2='https://api.parliament.uk/odata/Person(%27{}%27)/PersonHasIncumbency?$expand=IncumbencyHasPosition'.format(r2)
+            r3=requests.get(url2)
+            r4=r3.json()
+            for i in r4['value']:
+                try:
+                    d = datetime.strptime(i['IncumbencyStartDate'], "%Y-%m-%dT%H:%M:%SZ")
+                    datum = d.strftime("%d/%m/%Y")
+                    tw = i['IncumbencyHasPosition']['PositionName'] + ", started:" + datum
+
+                except TypeError:
+                    pass
+
+                                #print('------')
+                                #api.update_status(repl + " " + tw + " " + tim)
+                replyText = str("@"+screenName + " " + tw)
+                print replyText
+                try:
+                    api.update_status(status=replyText, in_reply_to_status_id = tweetId)
+                except tweepy.TweepError as e:
+                    pass
+        else:
+            #tv = n[z]['DateOfWrit'].value
+            #tw = str(tv)
+            replyText = str("@"+screenName + " I'll need a hashtag to tell you more about " + tr1 + " #committees or #govposts")
+            print replyText
+            try:
+                api.update_status(status=replyText, in_reply_to_status_id = tweetId)
+            except tweepy.TweepError as e:
+                pass
+        #api.update_status(repl + " " + tw)
+        #print(repl + " " + tw)
 
 if __name__ == '__main__':
     streamListener = ReplyToTweet()
     twitterStream = Stream(auth, streamListener)
     twitterStream.userstream(_with='user')
-    #app.run()
-
-
-# 
-#     #for tweet in tweepy.Cursor(api.search,
-#                                #q='@parldivisionbot').items(1):
-#     #for tweet in tweepy.StreamListener():
-#         #x = str(tweet.text)
-#         #repl = str("@"+tweet.user.screen_name)
-#         #tim = str(tweet.id)
-#         #print x
-#             y = str(tweetText[17:])
-#             print y
-#             ordering=core.CommonExpression.OrderByFromString("NameDisplayAs Desc")
-#             m.set_orderby(ordering)
-# 
-#             #Add in a filter "+y+"
-#             odfilter = core.CommonExpression.from_str("substringof('"+y+"',NameDisplayAs)")
-#             m.set_filter(odfilter)
-# 
-#             #View the response
-#             for p in m.itervalues():
-#                 print(p.key(), p['NameDisplayAs'].value)
-#                 z = int(p.key())
-#                 print z
-# 
-#             n=c.feeds['Members'].OpenCollection()
-#             mcm=n[z]['MemberGovernmentPosts'].OpenCollection()
-#             for k in mcm.keys():
-#                 #Get the Committee details
-#                 with mcm[k]['GovernmentPost'].OpenCollection() as mcmc:
-#                     for k2,v2 in mcmc[mcmc.keys()[0]].data_items():
-#                         #if k2=='NameDisplayAs' or k2=='HansardName' or k2=='StartDate':
-#                         if k2=='HansardName':
-#                             #or v2.value is None:
-#                             tw = str(v2.value)
-#                         else: 
-#                             tw = "No posts held"
-#                         print(v2.value)
-#                 #print('------')
-#             try:
-#                 #api.update_status(repl + " " + tw + " " + tim)
-#                 api.update_status(repl + " " + tw, in_reply_to_status_id = tim)
-#             except tweepy.error.TweepError:
-#                 pass
-#             #api.update_status(repl + " " + tw)
-#             #print(repl + " " + tw)
-# 
-# if __name__ == '__main__':
-#     streamListener = ReplyToTweet()
-#     twitterStream = Stream(auth, streamListener)
-#     twitterStream.userstream(_with='user')
-#         
-
-#     y = str(x[17:])
-#     print y
-#     ordering=core.CommonExpression.OrderByFromString("NameDisplayAs Desc")
-#     m.set_orderby(ordering)
-# 
-#     #Add in a filter "+y+"
-#     odfilter = core.CommonExpression.from_str("substringof('"+y+"',NameDisplayAs)")
-#     m.set_filter(odfilter)
-# 
-#     #View the response
-#     for p in m.itervalues():
-#         print(p.key(), p['NameDisplayAs'].value)
-#         z = int(p.key())
-#         print z
-
-#     n=c.feeds['Members'].OpenCollection()
-#     mcm=n[z]['MemberGovernmentPosts'].OpenCollection()
-#     for k in mcm.keys():
-#         #Get the Committee details
-#         with mcm[k]['GovernmentPost'].OpenCollection() as mcmc:
-#             for k2,v2 in mcmc[mcmc.keys()[0]].data_items():
-#                 #if k2=='NameDisplayAs' or k2=='HansardName' or k2=='StartDate':
-#                 if k2=='HansardName':
-#                     #or v2.value is None:
-#                     tw = str(v2.value)
-#                     print(v2.value)
-#         #print('------')
-
-# #api.update_status(repl + " " + tw)
-# print(repl + " " + tw)
-
