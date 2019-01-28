408cbbeeb3625ec0a26741159bc8ce857692f986
Mognar <philipbgorman@gmail.com>
2019-01-28 12:51:51 +0000

    Update Membersposts.py

--- Membersposts.py
+++ Membersposts.py
@@ -41,15 +41,15 @@ class ReplyToTweet(StreamListener):
     #@app.route("/")
     def on_status(self, status):
         if hasattr(status, 'retweeted_status'):
-			try:
-				tweet = status.retweeted_status.extended_tweet["full_text"]
-			except:
-				tweet = status.retweeted_status.text
-		else:
-			try:
-				tweet = status.extended_tweet["full_text"]
-			except AttributeError:
-				tweet = status.text
+		try:
+			tweet = status.retweeted_status.extended_tweet["full_text"]
+		except:
+			tweet = status.retweeted_status.text
+	else:
+		try:
+			tweet = status.extended_tweet["full_text"]
+		except AttributeError:
+			tweet = status.text
          id_str = status.id_str
          name = status.user.screen_name
         
